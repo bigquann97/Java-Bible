6b24783b731c1196ad4491a0eebf09d09856f8f2
Gwanho Kim <bigquann97@khu.ac.kr>
2021-11-12 20:54:43 +0900

    Second Commit

--- 14. 람다와 스트림/src/aLambda/LambdaEx2/LambdaEx2.java
+++ 14. 람다와 스트림/src/aLambda/LambdaEx2/LambdaEx2.java
@@ -5,10 +5,16 @@ interface MyFunction {
     void myMethod(); // public abstract void myMethod();
 }
 
+@FunctionalInterface
+interface MyFunction2 {
+    void myMethod();
+}
+
 public class LambdaEx2 {
     public static void main(String[] args) {
         MyFunction f = () -> {}; // MyFunction f = (MyFunction)( () -> {} );
         Object obj = (MyFunction)( () -> {} );
+        // Object obj = (Object)( () -> {} ); - 오류
         String str = ((Object)(MyFunction)( () -> {} )).toString();
 
         System.out.println(f);
@@ -18,7 +24,8 @@ public class LambdaEx2 {
 //        System.out.println( () -> {} ); - 에러 람다식은 Object타입 형변환 불가
         System.out.println((MyFunction)( () -> {} ));
 //        System.out.println((MyFunction)( () -> {} ).toString() ); - 에러
-        System.out.println(((Object)(MyFunction)( () -> {} )).toString());
+        System.out.println( ( (Object) (MyFunction) ( () -> {} ) ).toString() );
+        System.out.println();
     }
 
 }
