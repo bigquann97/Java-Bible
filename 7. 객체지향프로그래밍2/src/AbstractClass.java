c960287cc16127ae4f26a54df39564d939535b2b
Gwanho Kim <bigquann97@khu.ac.kr>
2021-11-20 17:35:19 +0900

    Commit at 17:35 (commit test)

--- 7. 객체지향프로그래밍2/src/AbstractClass.java
+++ 7. 객체지향프로그래밍2/src/AbstractClass.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class AbstractClass {
+public class AbstractClass {
+    public static void main(String[] args) {
+
+    }
+}
+
+abstract class Player {
+    boolean pause;
+    int currentPos;
+
+    Player() {
+        pause = false;
+        currentPos = 0;
+    }
+    abstract void play(int pos);
+    abstract void stop();
+
+    void play() {
+        play(currentPos);
+    }
+
+    void pause() {
+        if(pause) {
+            pause = false;
+            play(currentPos);
+        } else {
+            pause = true;
+            stop();
+        }
+
+    }
+}
+
+class Mp3Player extends Player {
+
+    @Override
+    void play(int pos) {
+
+    }
+
+    @Override
+    void stop() {
+
+    }
 }
